4fbf252e3acae7c381b5bcd8ce911759100f3755
Steph <stazatorres@hotmail.com>
2023-10-01 18:31:11 +0100

    added if statement, change manually in browser and stuff happens

--- node_crash_course/index.js
+++ node_crash_course/index.js
@@ -30,6 +30,17 @@ const server = http.createServer((req, res) => {
       }
     );
   }
+
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 });
 
 server.listen(PORT, () => {
