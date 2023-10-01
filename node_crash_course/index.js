7870d3ee41723f6b39a9980a0693e0aad251185c
Steph <stazatorres@hotmail.com>
2023-10-01 19:11:41 +0100

    server checks file type, and serves upto correct files with correct content-type, including css

--- node_crash_course/index.js
+++ node_crash_course/index.js
@@ -20,7 +20,7 @@ const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
+  /*    if (req.url === "/") {
     fs.readFile(
       path.join(__dirname, "public", "index.html"),
       (err, content) => {
@@ -31,16 +31,76 @@ const server = http.createServer((req, res) => {
     );
   }
 
-  if (req.url === "/about") {
-    fs.readFile(
-      path.join(__dirname, "public", "about.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end(content);
-      }
-    );
+  if (req.url === "/api/users") {
+    const users = [
+      {
+        name: "Bob",
+        age: 40,
+      },
+      {
+        name: "John",
+        age: 33,
+      },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }*/
+
+  //Build a file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  //Get extension of file so we can set content-type header
+  let extName = path.extname(filePath);
+
+  //set initial content type
+  let contentType = "text/html";
+
+  //check ext and set content type
+  switch (extName) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
   }
+  //surely a map would be a better way! loop through map, if extName is equal to map key, contentType = to value.
+
+  //Read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf-8");
+          }
+        );
+      } else {
+        //probably a server error, e.g 500
+        res.writeHead(500);
+        res.end(`Server error ${err.code}`);
+      }
+    } else {
+      //success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 server.listen(PORT, () => {
